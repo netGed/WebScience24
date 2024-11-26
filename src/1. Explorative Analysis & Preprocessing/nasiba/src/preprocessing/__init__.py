d42bad7353bacd078a77ce3c2b128db6d1cd7ec8
Nasiba Tuychieva <nasi_26@yahoo.de>
2024-11-26 23:45:04 +0100

    added new python generic functions/changed data cleaning notebooks structure

---
+++ src/1. Explorative Analysis & Preprocessing/nasiba/src/preprocessing/__init__.py
@@ -0,0 +1,4 @@
+from src.preprocessing.cleaning.clean_data_generic_functions import remove_special_characters, remove_url_from_tweet
+
+# __all__ list to define what is accessible when importing *
+__all__ = ["remove_special_characters", "remove_url_from_tweet"]
