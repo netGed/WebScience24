b7538e7af7108955f75219be1deaf58959def750
netGed <140313925+netGed@users.noreply.github.com>
2025-03-09 11:45:58 +0100

    Import aller Modelle für Webapp

--- webapp/backend/app/classifications.py
+++ webapp/backend/app/classifications.py
@@ -1,11 +1,11 @@
-import random
 import pandas as pd
 from keras.src.utils import pad_sequences
 from scipy.special import softmax
 
 from webapp.backend.app.models.models import tokenizer_roberta, model_roberta, tokenizer_bert, model_bert, \
     model_ensemble, tfidf_vectorizer_ensemble, tokenizer_gru, model_gru, max_len_gru, \
-    vectorize_w2v, w2v_vectorizer_nb, model_nb_w2v, threshold_gru
+    threshold_gru, model_nb, vectorizer_nb_tfidf, model_svm, vectorizer_svm_tfidf, vectorize_glove_test_data_predict, \
+    tokenizer_lstm, model_lstm, threshold_lstm
 
 
 def classify_with_ensemble(tweet):
@@ -34,14 +34,14 @@ def classify_with_ensemble(tweet):
 
 def classify_with_nb(tweet):
     # vectorize tweet
-    df = pd.DataFrame([tweet])
-    tweet_vectorized = vectorize_w2v(df[0], w2v_vectorizer_nb)
+    df = pd.DataFrame({'Tweet': [tweet]})
+    tweet_vectorized = vectorizer_nb_tfidf.transform([df['Tweet'][0]])
 
     # generate classification
-    probs = model_nb_w2v.predict_proba(tweet_vectorized)
+    probs = model_nb.predict_proba(tweet_vectorized)
     preds0 = round(probs[:, 0][0], 2)
     preds1 = round(probs[:, 1][0], 2)
-    pred = model_nb_w2v.predict(tweet_vectorized)[0]
+    pred = model_nb.predict(tweet_vectorized)[0]
 
     result = {
         "model_name": "Naive Bayes",
@@ -53,32 +53,21 @@ def classify_with_nb(tweet):
     return result
 
 
-# todo
 def classify_with_svm(tweet):
-    # load model
-
-    # clean tweet before vectorization
-
     # vectorize tweet
+    df = pd.DataFrame({'Tweet': [tweet]})
+    tweet_vectorized = vectorizer_svm_tfidf.transform([df['Tweet'][0]])
 
     # generate classification
-    # probs = model.predict_proba(tweet_vectorized)
+    # probs = model_svm.predict_proba(tweet_vectorized) => SVM hat keine predict_proba Funktion
     # preds0 = round(probs[:, 0][0], 2)
     # preds1 = round(probs[:, 1][0], 2)
-    # pred = model.predict(tweet_vectorized)[0]
-
-    # mock classification
-    preds0 = round(random.randint(0, 99) / 100, 2)
-    preds1 = round(1 - preds0, 2)
-    if preds0 > preds1:
-        pred = 0
-    else:
-        pred = 1
+    pred = model_svm.predict(tweet_vectorized)[0]
 
     result = {
         "model_name": "SVM",
-        "zero_probability": str(preds0),
-        "one_probability": str(preds1),
+        "zero_probability": "-",
+        "one_probability": "-",
         "label": str(pred),
     }
 
@@ -109,24 +98,20 @@ def classify_with_gru(tweet):
     return result
 
 
-# todo
 def classify_with_lstm(tweet):
-    # load model
-
-    # clean tweet before vectorization
-
     # vectorize tweet
+    X_data_vectorized = vectorize_glove_test_data_predict(
+        text=tweet,
+        tokenizer=tokenizer_lstm
+    )
 
     # generate classification
-    # probs = model.predict_proba(tweet_vectorized)
-    # preds0 = round(probs[:, 0][0], 2)
-    # preds1 = round(probs[:, 1][0], 2)
-    # pred = model.predict(tweet_vectorized)[0]
+    pred = model_lstm.predict(X_data_vectorized)[0][0]
 
     # mock classification
-    preds0 = round(random.randint(0, 99) / 100, 2)
-    preds1 = round(1 - preds0, 2)
-    if preds0 > 0.5:
+    preds0 = round(1 - pred, 2)
+    preds1 = round(pred, 2)
+    if preds0 > threshold_lstm:
         pred = 0
     else:
         pred = 1
--- webapp/backend/app/models/models.py
+++ webapp/backend/app/models/models.py
@@ -1,49 +1,36 @@
 import joblib
 import pickle
-
-import numpy as np
-from keras.src.saving import load_model
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, BertTokenizer
-from nltk import word_tokenize
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
+from tensorflow.keras.models import load_model
 
 # SVM
-model_svm = ""
-
-# RNN-LSTM
-model_lstm = ""
+vectorizer_svm_tfidf = joblib.load("models/svc/tfidf_vectorizer_svc.joblib")
+model_svm = joblib.load("models/svc/model_svc.joblib")
 
 # ENSEMBLE
 tfidf_vectorizer_ensemble = joblib.load("models/ensemble/tfidf_vectorizer_for_brf.joblib")
 model_ensemble = joblib.load("models/ensemble/tfidf_balancedrandomforest.joblib")
 
 # NAIVE BAYES
-w2v_vectorizer_nb = joblib.load("models/bayes/vectorizer_w2v.joblib")
-model_nb_w2v = joblib.load("models/bayes/model_nb_w2v.joblib")
+vectorizer_nb_tfidf = joblib.load("models/nb/vectorizer_nb_tfidf.joblib")
+model_nb = joblib.load("models/nb/model_nb_tfidf_comp.joblib")
 
-
-def vectorize_w2v(tweets, loaded_vectorizer, vector_size=300):
-    x_tokenized = tweets.map(word_tokenize)
-
-    def w2v_vector(x_tokenized, vector_size):
-        vec = np.zeros(vector_size).reshape((1, vector_size))
-        count = 0
-        for word in x_tokenized:
-            try:
-                vec += loaded_vectorizer.wv[word].reshape((1, vector_size))
-                count += 1
-            except KeyError:
-
-                continue
-        if count != 0:
-            vec /= count
-        return vec
-
-    tweets_w2v = np.zeros((len(x_tokenized), 300))
-    for i in range(len(x_tokenized)):
-        tweets_w2v[i, :] = w2v_vector(x_tokenized.iloc[i], 300)
-
-    return tweets_w2v
-
+# RNN-LSTM
+threshold_lstm = 0.35
+model_lstm = load_model("models/lstm/model_lstm_17.keras")
+with open("models/lstm/tokenizer_lstm.json", "r", encoding="utf-8") as f:
+    tokenizer_data = f.read()
+    tokenizer_lstm = tokenizer_from_json(tokenizer_data)
+lstm_glove_embeddings = {}
+with open("models/lstm/glove.6B.200d.txt", encoding='utf-8') as f:
+    for line in f:
+        values = line.split()
+        word = values[0]
+        vector = np.asarray(values[1:], dtype='float32')
+        lstm_glove_embeddings[word] = vector
 
 # RNN-GRU
 threshold_gru = 0.35
@@ -61,3 +48,43 @@ model_bert = AutoModelForSequenceClassification.from_pretrained(PATH_BERT_TUNED,
 PATH_ROBERTA_TUNED = f"models/roberta/roberta_hate_mixed_cleaned"
 tokenizer_roberta = AutoTokenizer.from_pretrained(PATH_ROBERTA_TUNED, local_files_only=True)
 model_roberta = AutoModelForSequenceClassification.from_pretrained(PATH_ROBERTA_TUNED, local_files_only=True)
+
+
+# spezielle LSTM-Vektorisierungsfunktion für einzelnen Tweet
+def vectorize_glove_test_data_predict(text, vector_size=200, max_seq_len=50, tokenizer=None):
+    """
+    Vektorisiert einen einzelnen Textstring mit vortrainierten GloVe-Embeddings.
+
+    Args:
+        text (str): Der Eingabetext, der vektorisiert werden soll.
+        glove_path (str): Pfad zur GloVe-Embeddings-Datei.
+        vector_size (int, optional): Größe der GloVe-Vektoren (Standard: 200).
+        max_seq_len (int, optional): Maximale Sequenzlänge für Padding (Standard: 50).
+        tokenizer (Tokenizer, optional): Der trainierte Tokenizer. Falls None, wird ein neuer erstellt.
+
+    Returns:
+        np.ndarray: Ein 3D-Array mit der Form `(1, max_seq_len, vector_size)`, das direkt für LSTM nutzbar ist.
+    """
+
+    if tokenizer is None:
+        raise ValueError("Ein trainierter Tokenizer muss übergeben werden!")
+
+    X_sequence = tokenizer.texts_to_sequences([text])  # Text in Sequenz umwandeln (Liste mit 1 Element)
+
+    X_padded = pad_sequences(X_sequence, padding='post', maxlen=max_seq_len)
+
+    def get_glove_vectors(sequence, glove_embeddings, vector_size, max_seq_len):
+        vectors = [glove_embeddings.get(tokenizer.index_word.get(idx, ''), np.zeros(vector_size)) for idx in sequence]
+
+        # Padding sicherstellen
+        if len(vectors) < max_seq_len:
+            vectors.extend([np.zeros(vector_size)] * (max_seq_len - len(vectors)))
+        else:
+            vectors = vectors[:max_seq_len]
+
+        return np.array(vectors, dtype=np.float32)
+
+    X_vectorized = np.array(get_glove_vectors(X_padded[0], lstm_glove_embeddings, vector_size, max_seq_len),
+                            dtype=np.float32)
+
+    return np.expand_dims(X_vectorized, axis=0)
